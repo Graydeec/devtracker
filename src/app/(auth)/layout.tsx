0c67f037926fd17f77c280b4bb576e79d92afd15
GrayDee <43388632+GrayDee@users.noreply.github.com>
2026-04-14 01:33:57 -0400

    01-authentication

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+// app/(auth)/layout.tsx
+import { ClerkProvider } from "@clerk/nextjs";
+import PublicHeader from "../../components/PublicHeader";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <main>{children}</main>;
+}
